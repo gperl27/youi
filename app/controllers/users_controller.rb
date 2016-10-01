f6f1948da66906488732de625ecfe8dabf778cde
Greg Perlman <gperlman27@gmail.com>
2016-10-01 12:43:05 -0400

    assign css properties to any given user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
-  # before_action :h1_props, only: [:index]
+  before_action :find_user, only: [:index, :show]
+  before_action :h1_props, only: [:index, :show]
+  before_action :h2_props, only: [:index, :show]
+  before_action :h3_props, only: [:index, :show]
+  before_action :h4_props, only: [:index, :show]
+  before_action :h5_props, only: [:index, :show]
+  before_action :button_props, only: [:index, :show]
+  before_action :paragraph_props, only: [:index, :show]
+  before_action :image_props, only: [:index, :show]
+  before_action :section_props, only: [:index, :show]
+  before_action :body_props, only: [:index, :show]
+  before_action :li_props, only: [:index, :show]
+
   def index
-    user = User.find_by(id: session[:user_id])
-    h1_props(user)  
-
-    @h1_selector_id = user.h1s[0].id
-
+    @h1_selector_id = @user.h1s[0].id
   end
 
   def create
@@ -14,7 +22,7 @@ class UsersController < ApplicationController
     user = User.create(cookie_id: 2)
     session[:user_id] = user.id
     user.initialize_user
-    
+
     redirect_to user_path
   end
 
@@ -28,14 +36,212 @@ class UsersController < ApplicationController
   end
 
   private
+  def find_user
+    @user = User.find_by(id: session[:user_id])
+  end
 
-  def h1_props(user)
-    h1 = user.h1s[0].h1_properties
+  def h1_props
+    h1 = @user.h1s[0].h1_properties
 
     # vote_count(h1)
     @h1_font_size = average(h1, "fontsize")
     @h1_font_weight = median(h1, "fontweight")
     @h1_text_transform = median(h1, "texttransformation")
     @h1_letter_spacing = average(h1, "letterspacing")
+  end
+
+  def h2_props
+    h2 = @user.h2s[0].h2_properties
+    # vote_count(h2)
+
+    @h2_font_size = average(h2, "fontsize")
+    @h2_font_weight = median(h2, "fontweight")
+    @h2_text_transform = median(h2, "texttransformation")
+    @h2_letter_spacing = average(h2, "letterspacing")
+    @h2_word_spacing = average(h2, "wordspacing")
+  end
+
+  def h3_props
+    h3 = @user.h3s[0].h3_properties
+    # vote_count(h3)
+
+    @h3_font_size = average(h3, "fontsize")
+    @h3_font_weight = median(h3, "fontweight")
+    @h3_text_transform = median(h3, "texttransformation")
+    @h3_letter_spacing = average(h3, "letterspacing")
+    @h3_word_spacing = average(h3, "wordspacing")
+  end
+
+  def h4_props
+    h4 = @user.h4s[0].h4_properties
+    # vote_count(h4)
+
+    @h4_font_size = average(h4, "fontsize")
+    @h4_font_weight = median(h4, "fontweight")
+    @h4_text_transform = median(h4, "texttransformation")
+    @h4_letter_spacing = average(h4, "letterspacing")
+    @h4_word_spacing = average(h4, "wordspacing")
+  end
+
+  def h5_props
+    h5 = @user.h5s[0].h5_properties
+    # vote_count(h5)
+
+    @h5_font_size = average(h5, "fontsize")
+    @h5_font_weight = median(h5, "fontweight")
+    @h5_text_transform = median(h5, "texttransformation")
+    @h5_letter_spacing = average(h5, "letterspacing")
+    @h5_word_spacing = average(h5, "wordspacing")
+  end
+
+  def button_props
+    button_1 = @user.buttons[0].button_properties
+    button_2 = @user.buttons[1].button_properties
+    button_3 = @user.buttons[2].button_properties
+
+      # vote_count(button)
+
+    @button_1_font_size = average(button_1, "fontsize")
+    @button_1_font_weight = median(button_1, "fontweight")
+    @button_1_text_transform = median(button_1, "texttransformation")
+    @button_1_letter_spacing = average(button_1, "letterspacing")
+    @button_1_padding = average(button_1, "padding")
+    @button_1_border = average(button_1, "border")
+    @button_1_border_radius = average(button_1, "borderradius")
+    @button_1_border_style = median(button_1, "border_style")
+    @button_1_color_hue = median(button_1, "color_hue")
+    @button_1_color_saturation = median(button_1, "color_saturation")
+    @button_1_color_luminosity = median(button_1, "color_luminosity")
+    @button_1_border_color_hue = median(button_1, "border_hue")
+    @button_1_border_color_saturation = median(button_1, "border_saturation")
+    @button_1_border_color_luminosity = median(button_1, "border_luminosity")
+    @button_1_bg_color_hue = median(button_1, "bg_hue")
+    @button_1_bg_color_saturation = median(button_1, "bg_saturation")
+    @button_1_bg_color_luminosity = median(button_1, "bg_luminosity")
+
+    @button_2_font_size = average(button_2, "fontsize")
+    @button_2_font_weight = median(button_2, "fontweight")
+    @button_2_text_transform = median(button_2, "texttransformation")
+    @button_2_letter_spacing = average(button_2, "letterspacing")
+    @button_2_padding = average(button_2, "padding")
+    @button_2_border = average(button_2, "border")
+    @button_2_border_radius = average(button_2, "borderradius")
+    @button_2_border_style = median(button_2, "border_style")
+    @button_2_color_hue = median(button_2, "color_hue")
+    @button_2_color_saturation = median(button_2, "color_saturation")
+    @button_2_color_luminosity = median(button_2, "color_luminosity")
+    @button_2_border_color_hue = median(button_2, "border_hue")
+    @button_2_border_color_saturation = median(button_2, "border_saturation")
+    @button_2_border_color_luminosity = median(button_2, "border_luminosity")
+    @button_2_bg_color_hue = median(button_2, "bg_hue")
+    @button_2_bg_color_saturation = median(button_2, "bg_saturation")
+    @button_2_bg_color_luminosity = median(button_2, "bg_luminosity")
+
+    @button_3_font_size = average(button_3, "fontsize")
+    @button_3_font_weight = median(button_3, "fontweight")
+    @button_3_text_transform = median(button_3, "texttransformation")
+    @button_3_letter_spacing = average(button_3, "letterspacing")
+    @button_3_padding = average(button_3, "padding")
+    @button_3_border = average(button_3, "border")
+    @button_3_border_radius = average(button_3, "borderradius")
+    @button_3_border_style = median(button_3, "border_style")
+    @button_3_color_hue = median(button_3, "color_hue")
+    @button_3_color_saturation = median(button_3, "color_saturation")
+    @button_3_color_luminosity = median(button_3, "color_luminosity")
+    @button_3_border_color_hue = median(button_3, "border_hue")
+    @button_3_border_color_saturation = median(button_3, "border_saturation")
+    @button_3_border_color_luminosity = median(button_3, "border_luminosity")
+    @button_3_bg_color_hue = median(button_3, "bg_hue")
+    @button_3_bg_color_saturation = median(button_3, "bg_saturation")
+    @button_3_bg_color_luminosity = median(button_3, "bg_luminosity")
+  end
+
+  def paragraph_props
+    paragraph_1 = @user.paragraphs[0].paragraph_properties
+    paragraph_2 = @user.paragraphs[1].paragraph_properties
+    # vote_count(paragraph_1)
+    # vote_count(paragraph_2)
+
+    @paragraph_1_font_size = average(paragraph_1, "fontsize")
+    @paragraph_1_text_align = median(paragraph_1, "textalign")
+    @paragraph_1_padding = median(paragraph_1, "padding")
+    @paragraph_1_indent = average(paragraph_1, "indent")
+    @paragraph_1_word_spacing = average(paragraph_1, "wordspacing")
+
+    @paragraph_2_font_size = average(paragraph_2, "fontsize")
+    @paragraph_2_text_align = median(paragraph_2, "textalign")
+    @paragraph_2_padding = median(paragraph_2, "padding")
+    @paragraph_2_indent = average(paragraph_2, "indent")
+    @paragraph_2_word_spacing = average(paragraph_2, "wordspacing")
+  end
+
+  def image_props
+    image = @user.images[0].image_properties
+    # vote_count(image)
+
+    @image_padding = average(image, "padding")
+    @image_border = average(image, "border")
+    @image_border_radius = average(image, "borderradius")
+    @image_height = average(image, "height")
+    @image_width = average(image, "width")
+    @image_border_color_hue = median(image, "border_hue")
+    @image_border_color_saturation = median(image, "border_saturation")
+    @image_border_color_luminosity = median(image, "border_luminosity")
+  end
+
+  def li_props
+    li = @user.listelements[0].listelement_properties
+    # vote_count(li)
+
+    @li_font_size = average(li, "fontsize")
+    @li_text_transform = median(li, "texttransformation")
+    @li_letter_spacing = average(li, "letterspacing")
+    @li_font_style = median(li, "fontstyle")
+  end
+
+  def section_props
+    section = @user.sections[0].section_properties
+    section_2 = @user.sections[1].section_properties
+    section_3 = @user.sections[2].section_properties
+    section_4 = @user.sections[3].section_properties
+
+    @section_1_color_hue = median(section, "color_hue")
+    @section_1_color_saturation = median(section, "color_saturation")
+    @section_1_color_luminosity = median(section, "color_luminosity")
+    @section_1_bg_color_hue = median(section, "bg_hue")
+    @section_1_bg_color_saturation = median(section, "bg_saturation")
+    @section_1_bg_color_luminosity = median(section, "bg_luminosity")
+
+    @section_2_color_hue = median(section_2, "color_hue")
+    @section_2_color_saturation = median(section_2, "color_saturation")
+    @section_2_color_luminosity = median(section_2, "color_luminosity")
+    @section_2_bg_color_hue = median(section_2, "bg_hue")
+    @section_2_bg_color_saturation = median(section_2, "bg_saturation")
+    @section_2_bg_color_luminosity = median(section_2, "bg_luminosity")
+
+    @section_3_color_hue = median(section_3, "color_hue")
+    @section_3_color_saturation = median(section_3, "color_saturation")
+    @section_3_color_luminosity = median(section_3, "color_luminosity")
+    @section_3_bg_color_hue = median(section_3, "bg_hue")
+    @section_3_bg_color_saturation = median(section_3, "bg_saturation")
+    @section_3_bg_color_luminosity = median(section_3, "bg_luminosity")
+
+    @section_4_color_hue = median(section_4, "color_hue")
+    @section_4_color_saturation = median(section_4, "color_saturation")
+    @section_4_color_luminosity = median(section_4, "color_luminosity")
+    @section_4_bg_color_hue = median(section_4, "bg_hue")
+    @section_4_bg_color_saturation = median(section_4, "bg_saturation")
+    @section_4_bg_color_luminosity = median(section_4, "bg_luminosity")
+  end
+
+  def body_props
+    headingfont = @user.bodies[0].body_properties
+    bodyfont = @user.bodies[1].body_properties
+
+    heading_font = median(headingfont, "fontfamily")
+    body_font = median(bodyfont, "fontfamily")
+
+    @heading_font = clean_font(heading_font)
+    @body_font = clean_font(body_font)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,81 +17,81 @@ class User < ApplicationRecord
     h1 = self.h1s.create!
     h1.h1_properties.create!(fontsize: 36, letterspacing: 4, texttransformation: "capitalize", fontweight: 700)
 
-    # h2 = self.h2s.create!
-    # h2.h2_properties.create!(fontsize: 32, letterspacing: 2, texttransformation: "lowercase", fontweight: 400, wordspacing: 5)
+    h2 = self.h2s.create!
+    h2.h2_properties.create!(fontsize: 32, letterspacing: 2, texttransformation: "lowercase", fontweight: 400, wordspacing: 5)
 
-    # h3 = self.h3s.create!
-    # h3.h3_properties.create!(fontsize: 28, letterspacing: 3, texttransformation: "uppercase", fontweight: 300, wordspacing: 3)
+    h3 = self.h3s.create!
+    h3.h3_properties.create!(fontsize: 28, letterspacing: 3, texttransformation: "uppercase", fontweight: 300, wordspacing: 3)
 
-    # h4 = self.h4s.create!
-    # h4.h4_properties.create!(fontsize: 22, letterspacing: 1, texttransformation: "uppercase", fontweight: 300, wordspacing: 3)
+    h4 = self.h4s.create!
+    h4.h4_properties.create!(fontsize: 22, letterspacing: 1, texttransformation: "uppercase", fontweight: 300, wordspacing: 3)
 
-    # h5 = self.h5s.create!
-    # h5.h5_properties.create!(fontsize: 14, letterspacing: 2, texttransformation: "capitalize", fontweight: 200, wordspacing: 2)
+    h5 = self.h5s.create!
+    h5.h5_properties.create!(fontsize: 14, letterspacing: 2, texttransformation: "capitalize", fontweight: 200, wordspacing: 2)
 
-    # button_1 = self.buttons.create!
-    # button_1.button_properties.create!(
-    #   fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
-    #     border: 5, borderradius: 10, border_style: "solid", border_hue: 50, border_saturation: 50, border_luminosity: 50,
-    #     bg_hue: 0, bg_saturation: 100, bg_luminosity: 100,
-    #     color_hue: 0, color_saturation: 0, color_luminosity: 0
-    #   )
+    button_1 = self.buttons.create!
+    button_1.button_properties.create!(
+      fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
+        border: 5, borderradius: 10, border_style: "solid", border_hue: 50, border_saturation: 50, border_luminosity: 50,
+        bg_hue: 0, bg_saturation: 100, bg_luminosity: 100,
+        color_hue: 0, color_saturation: 0, color_luminosity: 0
+      )
 
-    # button_2 = self.buttons.create!
-    # button_2.button_properties.create!(
-    #   fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
-    #     border: 5, borderradius: 10, border_style: "solid", border_hue: 51, border_saturation: 51, border_luminosity: 51,
-    #     bg_hue: 1, bg_saturation: 99, bg_luminosity: 99,
-    #     color_hue: 1, color_saturation: 1, color_luminosity: 1
-    #   )
+    button_2 = self.buttons.create!
+    button_2.button_properties.create!(
+      fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
+        border: 5, borderradius: 10, border_style: "solid", border_hue: 51, border_saturation: 51, border_luminosity: 51,
+        bg_hue: 1, bg_saturation: 99, bg_luminosity: 99,
+        color_hue: 1, color_saturation: 1, color_luminosity: 1
+      )
 
-    # button_3 = self.buttons.create!
-    # button_3.button_properties.create!(
-    #   fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
-    #     border: 5, borderradius: 10, border_style: "solid", border_hue: 52, border_saturation: 52, border_luminosity: 52,
-    #     bg_hue: 2, bg_saturation: 98, bg_luminosity: 98,
-    #     color_hue: 2, color_saturation: 2, color_luminosity: 2
-    #   )
+    button_3 = self.buttons.create!
+    button_3.button_properties.create!(
+      fontsize: 16, texttransformation: "capitalize", fontweight: 200, letterspacing: 3, padding: 15,
+        border: 5, borderradius: 10, border_style: "solid", border_hue: 52, border_saturation: 52, border_luminosity: 52,
+        bg_hue: 2, bg_saturation: 98, bg_luminosity: 98,
+        color_hue: 2, color_saturation: 2, color_luminosity: 2
+      )
 
 
-    # li = self.listelements.create!
-    # li.listelement_properties.create!(fontsize: 14, letterspacing: 6, texttransformation: "Capitalize", fontstyle: "italic")
+    li = self.listelements.create!
+    li.listelement_properties.create!(fontsize: 14, letterspacing: 6, texttransformation: "Capitalize", fontstyle: "italic")
 
-    # paragraph = self.paragraphs.create!
-    # paragraph.paragraph_properties.create!(textalign: "left", fontsize: 12, wordspacing: 3, padding: 5, indent: 25)
+    paragraph = self.paragraphs.create!
+    paragraph.paragraph_properties.create!(textalign: "left", fontsize: 12, wordspacing: 3, padding: 5, indent: 25)
 
-    # paragraph2 = self.paragraphs.create!
-    # paragraph2.paragraph_properties.create!(textalign: "center", fontsize: 14, wordspacing: 4, padding: 10, indent: 15)
+    paragraph2 = self.paragraphs.create!
+    paragraph2.paragraph_properties.create!(textalign: "center", fontsize: 14, wordspacing: 4, padding: 10, indent: 15)
 
-    # image = self.images.create!
-    # image.image_properties.create!(height: 250, width: 250, border: 1, borderradius: 5, padding: 15, 
-    #   border_hue: 180, border_luminosity: 50, border_saturation: 50
-    #   )
+    image = self.images.create!
+    image.image_properties.create!(height: 250, width: 250, border: 1, borderradius: 5, padding: 15, 
+      border_hue: 180, border_luminosity: 50, border_saturation: 50
+      )
 
-    # section_1 = self.sections.create!
-    # section_1.section_properties.create!(
-    #   color_hue: 0, color_luminosity: 0, color_saturation: 0,
-    #   bg_hue: 0, bg_luminosity: 100, bg_saturation: 100,
-    #   )
-    # section_2 = self.sections.create!
-    # section_2.section_properties.create!(
-    #   color_hue: 1, color_luminosity: 1, color_saturation: 1,
-    #   bg_hue: 1, bg_luminosity: 99, bg_saturation: 99,
-    #   )
-    # section_3 = self.sections.create!
-    # section_3.section_properties.create!(
-    #   color_hue: 2, color_luminosity: 2, color_saturation: 2,
-    #   bg_hue: 2, bg_luminosity: 98, bg_saturation: 98,
-    #   )
-    # section_4 = self.sections.create!
-    # section_4.section_properties.create!(
-    #   color_hue: 3, color_luminosity: 3, color_saturation: 3,
-    #   bg_hue: 3, bg_luminosity: 97, bg_saturation: 97,
-    #   )
+    section_1 = self.sections.create!
+    section_1.section_properties.create!(
+      color_hue: 0, color_luminosity: 0, color_saturation: 0,
+      bg_hue: 0, bg_luminosity: 100, bg_saturation: 100,
+      )
+    section_2 = self.sections.create!
+    section_2.section_properties.create!(
+      color_hue: 1, color_luminosity: 1, color_saturation: 1,
+      bg_hue: 1, bg_luminosity: 99, bg_saturation: 99,
+      )
+    section_3 = self.sections.create!
+    section_3.section_properties.create!(
+      color_hue: 2, color_luminosity: 2, color_saturation: 2,
+      bg_hue: 2, bg_luminosity: 98, bg_saturation: 98,
+      )
+    section_4 = self.sections.create!
+    section_4.section_properties.create!(
+      color_hue: 3, color_luminosity: 3, color_saturation: 3,
+      bg_hue: 3, bg_luminosity: 97, bg_saturation: 97,
+      )
 
-    # body = self.bodies.create!
-    # heading = self.bodies.create!
-    # bodyfont = body.body_properties.create!(fontfamily: "Times New Roman")
-    # headingfont = heading.body_properties.create!(fontfamily: "Helvetica")
+    body = self.bodies.create!
+    heading = self.bodies.create!
+    bodyfont = body.body_properties.create!(fontfamily: "Times New Roman")
+    headingfont = heading.body_properties.create!(fontfamily: "Helvetica")
   end
 end
